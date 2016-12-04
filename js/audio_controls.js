04621d45d96da61407856ffb92e456351609a386
Andy Linton <asjl@lpnz.org>
2016-12-04 15:18:31 +1300

    Tidy up js libs

--- js/audio_controls.js
+++ js/audio_controls.js
@@ -1,34 +1,43 @@
+/*
+ * Audio controls for the browser audio player
+ * Developed as part of websites for http://session.nz and http://wellington.session.nz
+ * by Ted Cizadlo and Andy Linton
+ * Code available at:
+ * https://github.com/slow-session/session.nz/blob/master/js/audio_controls.js
+ * Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) Licence.
+ */
+
 function setPlaySpeed(speed,x) {
-    //alert("Speed = "+speed+", x= "+x + " "+document.getElementById(x).id);
     x.playbackRate = speed;
 }
 
 var BeginLoopTime = 0;
 var EndLoopTime = 0;
-var PreviousAudio=null;
-var PreviousB1=null;
-var PreviousB2=null;
-function SetPlayRange(audio,BE,button1,button2) {
+var PreviousAudio = null;
+var PreviousButton1 = null;
+var PreviousButton2 = null;
+
+function SetPlayRange(audio,BeginEnd,button1,button2) {
     if(audio.readyState == 0) return;
     if(PreviousAudio != audio) {
         BeginLoopTime = 0;
         EndLoopTime = 0;
-        if(PreviousB1 != null) {
+        if(PreviousButton1 != null) {
             var zero=0;
-            PreviousB1.value = zero.toFixed(1);
-            PreviousB2.value = PreviousAudio.duration.toFixed(1);
+            PreviousButton1.value = zero.toFixed(1);
+            PreviousButton2.value = PreviousAudio.duration.toFixed(1);
             // Consider deregistering the function here
         }    
 
         PreviousAudio = audio;
-        PreviousB1 = button1;
-        PreviousB2 = button2;
+        PreviousButton1 = button1;
+        PreviousButton2 = button2;
     }
 
     if (EndLoopTime == 0) {
         EndLoopTime=audio.duration-.25; // sampling rate is ~ every 250 ms (overshoot the end)
     }
-    if(BE == 0) {
+    if(BeginEnd == 0) {
         BeginLoopTime = audio.currentTime;
         button1.value = BeginLoopTime.toFixed(1);
         button2.value = EndLoopTime.toFixed(1);
@@ -38,7 +47,7 @@ function SetPlayRange(audio,BE,button1,button2) {
             }}, false);
         return;
      }
-     if(BE == 1) {
+     if(BeginEnd == 1) {
         EndLoopTime = audio.currentTime;
         button1.value = BeginLoopTime.toFixed(1);
         button2.value = EndLoopTime.toFixed(1);
@@ -48,7 +57,7 @@ function SetPlayRange(audio,BE,button1,button2) {
             }}, false);
         return;
     }
-    if(BE == 2 ) {
+    if(BeginEnd == 2 ) {
         BeginLoopTime = 0;
         EndLoopTime = audio.duration - .25;
         button1.value = BeginLoopTime.toFixed(1);
