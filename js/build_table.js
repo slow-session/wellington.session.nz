770069993c673e00d05e3fee76ff9944d9360d37
Andy Linton <asjl@lpnz.org>
2018-05-29 12:22:23 +1200

    Just show Wellington tunes to keep the table smaller

--- js/build_table.js
+++ js/build_table.js
@@ -4,7 +4,7 @@
   * Version: 1.0
   * Date: 7 Dec 2016
   *
-  * Developed as part of websites for https://wellington.session.nz
+  * Developed as part of websites for http://session.nz and http://wellington.session.nz
   * by Ted Cizadlo and Andy Linton
   * Code available at:
   * https://github.com/slow-session/session.nz/blob/master/js/audioID_controls.js
@@ -123,14 +123,18 @@
          }
      }
      var location = getQueryVariable('location');
-     if (location) {
-         searchTerm += location;
-         var e = document.getElementById('location-box');
-         if (e) {
-             e.value = location;
-         }
+     // default to showing Wellington tunes
+     if (!location) {
+         location = 'wellington';
      }
-
+     if (location != 'alltunes') {
+         searchTerm += location;
+     }
+     var e = document.getElementById('location-box');
+     if (e) {
+         e.value = location;
+     }
+    
      // Define the index terms for lunr search
      var tuneIndex = lunr(function() {
          this.field('id');
