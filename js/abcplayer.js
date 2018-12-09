beaef5d71e4bfe7568098618cc8dd227fe90d809
Andy Linton <asjl@lpnz.org>
2018-12-10 09:09:57 +1300

    Minor change to players

--- js/abcplayer.js
+++ js/abcplayer.js
@@ -59,9 +59,10 @@ function createABCplayer (tuneID, playerClass, timbre) {
     // Col 3
     abcPlayer += '      <div class="small-5 columns">';
     abcPlayer += '      <div class="speedControl">';
+    abcPlayer += '          <span title="Adjust playback speed with slider">';
     abcPlayer += '      <input name="flevel" id="RS' + tuneID + '" class="slider" type="range" min="50" max="120" value="100"';
     abcPlayer += '          onchange="changeABCspeed(ABC' + tuneID + ', pButton' + tuneID + ', value)">';
-    abcPlayer += '      <p class="audio">Speed - <output name="level">100</output>%</p>';
+    abcPlayer += '      <p class="audio">Speed - <strong><output name="level">100</output>%</strong></p>';
     abcPlayer += '      </div>';
     abcPlayer += '    </div>';
     abcPlayer += '  </div>';
