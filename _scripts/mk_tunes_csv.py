3128c8848c40f661facb78241e5494de809e91de
Andy Linton <asjl@lpnz.org>
2019-04-03 15:09:46 +1300

    tidy code base

--- _scripts/mk_tunes_csv.py
+++ _scripts/mk_tunes_csv.py
@@ -10,50 +10,50 @@ import os
 # You will need to change these variables to
 # match your directory structure
 #
-tunesDir = '/Users/asjl/GitHub/session.nz/_tunes'
-tuneURL = 'http://session.nz/tunes/'
-index = '/Users/asjl/Desktop/session.nz.index.csv'
+tunesDir = '/Users/asjl/GitHub/wellington.session.nz/_tunes'
+tuneURL = 'https://wellington.session.nz/tunes/'
+index = '/Users/asjl/Desktop/wellington.session.nz.index.csv'
 
 os.chdir(tunesDir)
 with open(index, 'w') as outfile:
     outfile.write('Title,URL,Arrowtown,Wellington,Dunedin,Auckland,Hamilton,\n')
-    for dir in os.listdir("."):
-        if os.path.isfile(dir):
+    for file in os.listdir("."):
+        if os.path.isdir(file):
             continue
-        for file in os.listdir(dir):
-            if file.endswith(".md"):
-                with open(tunesDir + '/' + dir + '/' + file, 'r') as infile:
-                    outline = ''
-                    for line in infile:
-                        if line.startswith('title:'):
-                            title = line.split(':', 2)[1].strip()
-                            outline = title.split(',')[0] + ','
-                            url = tuneURL + dir + '/' + file + ','
-                            outline += url.replace('.md', '.html')
-                            next
-                        if line.startswith('location:'):
-                            towns = line.split(':', 2)[1].rstrip()
-                            if 'Arrowtown' in towns:
-                                outline += 'x,'
-                            else:
-                                outline += ','
-                            if 'Wellington' in towns:
-                                outline += 'x,'
-                            else:
-                                outline += ','
-                            if 'Dunedin' in towns:
-                                outline += 'x,'
-                            else:
-                                outline += ','
-                            if 'Auckland' in towns:
-                                outline += 'x,'
-                            else:
-                                outline += ','
-                            if 'Hamilton' in towns:
-                                outline += 'x,'
-                            else:
-                                outline += ','
-                            next
-                            outline += '\n'
+        print(file)
+        if file.endswith(".md"):
+            with open(tunesDir + '/' + file, 'r') as infile:
+                outline = ''
+                for line in infile:
+                    if line.startswith('title:'):
+                        title = line.split(':', 2)[1].strip()
+                        outline = title.split(',')[0] + ','
+                        url = tuneURL + file + ','
+                        outline += url.replace('.md', '.html')
+                        next
+                    if line.startswith('location:'):
+                        towns = line.split(':', 2)[1].rstrip()
+                        if 'Arrowtown' in towns:
+                            outline += 'x,'
+                        else:
+                            outline += ','
+                        if 'Wellington' in towns:
+                            outline += 'x,'
+                        else:
+                            outline += ','
+                        if 'Dunedin' in towns:
+                            outline += 'x,'
+                        else:
+                            outline += ','
+                        if 'Auckland' in towns:
+                            outline += 'x,'
+                        else:
+                            outline += ','
+                        if 'Hamilton' in towns:
+                            outline += 'x,'
+                        else:
+                            outline += ','
+                        next
+                        outline += '\n'
 
-                    outfile.write(outline)
+                outfile.write(outline)
--- js/build_table_tunes_archive.js
+++ js/build_table_tunes_archive.js
@@ -7,7 +7,7 @@
   * Developed as part of websites for http://session.nz and http://wellington.session.nz
   * by Ted Cizadlo and Andy Linton
   * Code available at:
-  * https://github.com/slow-session/session.nz/blob/master/js/audioID_controls.js
+  * https://github.com/slow-session/wellington.session.nz/blob/master/js/audioID_controls.js
   * Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) Licence.
   *
   * Derived from: http://jekyll.tips/jekyll-casts/jekyll-search-using-lunr-js/
